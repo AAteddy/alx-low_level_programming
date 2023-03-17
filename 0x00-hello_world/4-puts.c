341166e9b9e9677306fd3a070e5d5fc6d8853d1b
Tedros <tedsaasfaha@gmail.com>
2023-03-17 05:15:13 -0700

    final update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+  *main - built-in function
+  *return - zero if it is success
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
