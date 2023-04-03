4dfa4d6eaab6511c5131a5c999ed6f66ee3ac819
Tedros <tedsaasfaha@gmail.com>
2023-04-03 08:32:18 -0700

    update the variables of 1-memcpy.c file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	int i;
+	int m = n;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < m; i++)
 	{
 		dest[i] = src[i];
+		n--;
 	}
 	return (dest);
 }
