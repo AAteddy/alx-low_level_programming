414d0508fd101c7a0d173a24a286bf5fbb4e5363
Tedros <tedsaasfaha@gmail.com>
2023-04-05 07:56:07 -0700

    update the 4-pow_recursion.c file

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,19 +9,11 @@
   */
 int _pow_recursion(int x, int y)
 {
-	int power = x;
-
 	if (y < 0)
-	{
 		return (-1);
-	}
-	else if (y == 0)
+	if (y > 0)
 	{
-		return (0);
+		return (x * _pow_recursion(x, y - 1));
 	}
-	else
-	{
-		power *= _pow_recursion(x, y - 1);
-	}
-	return (power);
+	return (1);
 }
