d8caeaba74546625f4f85e8f1c88fd306d98fe84
Tedros <tedsaasfaha@gmail.com>
2023-04-07 05:16:03 -0700

    update the file 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#define UNUSED(x) (void)(x)
 /**
   *main - a program that prints its name, followed by a new line
   *@argc: number of arguments which are supplied to the program
@@ -8,8 +7,9 @@
   *
   *Return: always 0
   */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	UNUSED(argc);
 	printf("%s\n", argv[0]);
+
+	return (0);
 }
