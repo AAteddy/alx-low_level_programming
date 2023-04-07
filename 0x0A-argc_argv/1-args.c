c9f5f11beaf56afff71fbb521e12d5b99b35dfda
Tedros <tedsaasfaha@gmail.com>
2023-04-07 05:27:27 -0700

    update the main argv of the 1-args.c file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
   *
   *Return: always 0 (success)
   */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
 
