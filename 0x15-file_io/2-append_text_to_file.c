5a74289c152ea9df1d7da4b8b72aefaa871823f6
AAteddy <tedsaasfaha@gmail.com>
2023-05-08 10:03:19 -0700

    updated the code by rewriting O_RDWR to the file 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	opn = open(filename, O_WRONLY | O_APPEND);
+	opn = open(filename, O_RDWR | O_APPEND);
 	wrt = write(opn, text_content, length);
 
 	if (opn == -1 || wrt == -1)
