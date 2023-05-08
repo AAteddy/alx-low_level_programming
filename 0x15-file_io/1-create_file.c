395b8187245e108290226a89efa3e0b8ff5cfc2e
AAteddy <tedsaasfaha@gmail.com>
2023-05-08 09:27:10 -0700

    updated the code by rewriting 'O_CREAT' correctly for file 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	opn = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(opn, text_content, length);
 
 	if (opn == -1 || wrt == -1)
