5de76428075081c2ca5d391c588b79fee2f58f9d
AAteddy <tedsaasfaha@gmail.com>
2023-05-11 11:10:14 -0700

    updated the for loop function code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,38 +1,34 @@
 #include "main.h"
 
 /**
-  * append_text_to_file - a function that appends text at the
-  *                       end of a file
-  * @filename: a pointer to the name of the file
-  * @text_content: is the NULL terminated string to add
-  *                at the end of the file
-  *
-  * Return: if the file exists (on success) 1,
-  *         if the file desnt exist or filename is NULL return -1
-  *         if you do not have the required permissions
-  *         to write the file return -1
-  */
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
+ *
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int opn, wrt;
-	int length = 0;
+	int opn, wrt, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
-			length++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	opn = open(filename, O_WRONLY | O_APPEND);
-	wrt = write(opn, text_content, length);
+	wrt = write(opn, text_content, len);
 
 	if (opn == -1 || wrt == -1)
 		return (-1);
 
 	close(opn);
 
-	return (-1);
+	return (1);
 }
