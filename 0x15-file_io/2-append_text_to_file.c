e786c99e36dc05cab76c528df6fe0d277f333ae7
AAteddy <tedsaasfaha@gmail.com>
2023-05-11 12:04:51 -0700

    updated the code by removing white space

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,30 +14,25 @@
   */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int opn, wrt, length = 0;
-	
+	int opn, wrt, len = 0;
+
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
+		for (len = 0; text_content[len];)
 		{
-			length++;
+			len++;
 		}
 	}
-
 	opn = open(filename, O_WRONLY | O_APPEND);
-	wrt = write(opn, text_content, length);
-
+	wrt = write(opn, text_content, len);
 	if (opn == -1 || wrt == -1)
 	{
 		return (-1);
 	}
-
 	close(opn);
-
 	return (1);
 }
